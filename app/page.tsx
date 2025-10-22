bc45c9c9a76e06308d9416710f8070a7f53b7aa9
Jarren San Jose <sanjose.jarren@gmail.com>
2025-10-22 02:29:56 -0700

    feat: add clerk waitlist

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Waitlist } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <h1>play to feel</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-bold mb-8">play to feel</h1>
+      <Waitlist />
+    </div>
   );
 }
